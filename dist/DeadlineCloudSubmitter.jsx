4e6a947555096c29adb94b4ebc6896261627d200
Alex Hughes <ahughesalex@gmail.com>
2025-06-24 16:44:09 -0700

    Rebuild DeadlineCloudSubmitter.jsx
    Signed-off-by: Alex Hughes <ahughesalex@gmail.com>

--- dist/DeadlineCloudSubmitter.jsx
+++ dist/DeadlineCloudSubmitter.jsx
@@ -2694,8 +2694,7 @@ function buildUI(thisObj) {
     listGroup.alignment = ['fill', 'fill'];
     listGroup.alignChildren = ['fill', 'fill']
 
-    const bounds = list == null ? undefined : list.bounds;
-    var list = listGroup.add("listbox", bounds, "", {
+    var list = listGroup.add("listbox", undefined, "", {
         multiselect: true,
         numberOfColumns: 4,
         showHeaders: true,
